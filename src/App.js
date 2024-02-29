819fc2b4a6f2a6ddb1669ac1939d2bb09fe61393
rajon-git <rajon.zhsust15@gmail.com>
2024-02-29 11:02:41 +0600

    botstrap needed file added and also some pages add to route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import './App.css';
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
 function App() {
-  return <h1>Hello from app</h1>
+  return (
+    <>
+     <BrowserRouter>
+       <Routes>
+         <Route path='/' element={<Layout/>}>
+          <Route index element={<Home/>}/>
+          <Route path='about' element={<About/>}/>
+          <Route path='contact' element={<Contact/>}/>
+         </Route>
+       </Routes>
+     </BrowserRouter>
+    </>
+  )
 }
 
 export default App;
