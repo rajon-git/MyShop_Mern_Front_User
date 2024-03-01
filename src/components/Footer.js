250353ff47b27eb9c8d536848a70e329bd4198b5
rajon-git <rajon.zhsust15@gmail.com>
2024-03-01 22:15:25 +0600

    footer last part complete

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,21 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <> 
+     <footer className='py-3'></footer>
+     <footer className='py-3'></footer>
+     <footer className='py-4'>
+      <div className='container-xxl'>
+        <div className='row'>
+          <div className='col-12'>
+            <p className='text-center  mb-0 text-white'>
+              &copy; {new Date().getFullYear()}; powered by Rajon
+            </p>
+          </div>
+        </div>
+      </div>
+     </footer>
+    </>
   )
 }
 
