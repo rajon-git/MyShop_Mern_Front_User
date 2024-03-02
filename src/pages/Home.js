a3ada2a48b5ae4a7c5e61cd0ff73ebd4bd447f98
rajon-git <rajon.zhsust15@gmail.com>
2024-03-02 13:01:40 +0600

    home wrapper section 2 added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,7 +8,7 @@ function Home() {
        <div className='container-xxl'>
         <div className='row'>
           <div className='col-6'>
-            <div className='main-banner position-relative p-3'>
+            <div className='main-banner position-relative'>
               <img src='images/main-banner-1.jpg' className='img-fluid rounded-3' alt='main banner'/>
             <div className='main-banner-content position-absolute'>
               <h4>Super chraged for pros</h4>
@@ -57,6 +57,52 @@ function Home() {
           </div>
         </div>
        </div>
+     </section>
+
+     <section className='home-wrapper-2 py-5'>
+      <div className='container-xxl'>
+        <div className='row'>
+          <div className='col-12'>
+            <div className='services d-flex align-items-center justify-content-between'>
+              <div className='d-flex align-items-center gap-15'>
+                 <img src='images/service.png' alt='services'/>
+                 <div>
+                 <h6>Free Shipping</h6>
+                 <p className='mb-0'>From all orders over $100</p>
+                 </div>
+              </div>
+              <div className='d-flex align-items-center gap-15'>
+                 <img src='images/service-02.png' alt='services'/>
+                 <div>
+                 <h6>Daily Surprize Offer</h6>
+                 <p className='mb-0'>Save upto 25%</p>
+                 </div>
+              </div>
+              <div className='d-flex align-items-center gap-15'>
+                 <img src='images/service-03.png' alt='services'/>
+                 <div>
+                 <h6>Support 24/7</h6>
+                 <p className='mb-0'>Shop with an expert</p>
+                 </div>
+              </div>
+              <div className='d-flex align-items-center gap-15'>
+                 <img src='images/service-04.png' alt='services'/>
+                <div>
+                <h6>Affordable Prices</h6>
+                 <p className='mb-0'>Get Factory Default Proces</p>
+                </div>
+              </div>
+              <div className='d-flex align-items-center gap-15'>
+                 <img src='images/service-05.png' alt='services'/>
+                 <div>
+                 <h6>Secure Payments</h6>
+                 <p className='mb-0'>100% Proteced Payments</p>
+                 </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
      </section>
     </>
   )
