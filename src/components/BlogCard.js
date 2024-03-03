a67412b72710860a15a4075a7226ce8130156494
rajon-git <rajon.zhsust15@gmail.com>
2024-03-03 23:36:49 +0600

    in header section blog are complete

---
+++ src/components/BlogCard.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function BlogCard() {
+  return (
+    <div className='col-3'>
+      <div className='blog-card'>
+        <div className='card-image'>
+            <img src='images/blog-1.jpg' className='img-fluid' alt='blog'/>
+        </div>
+        <div className='blog-content'>
+            <p className='date'>
+                3 March, 2024
+            </p>
+            <h5 className='title'>
+                A beautiful Friday Morning Renaissance
+            </h5>
+            <p className='desc'>
+            It is a long established fact that a reader 
+            will be distracted by the readable  
+            </p>
+            <Link to="/" className='button'>Read More</Link>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default BlogCard
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import Marquee from "react-fast-marquee";
+import BlogCard from '../components/BlogCard';
 
 function Home() {
   return (
@@ -214,6 +215,17 @@ function Home() {
             </Marquee>
             </div>
           </div>
+        </div>
+      </div>
+     </section>
+
+     <section className='blog-wrapper py-5 home-wrapper-2'>
+      <div className='container-xxl'>
+        <div className='row'>
+          <BlogCard/>
+          <BlogCard/>
+          <BlogCard/>
+          <BlogCard/>
         </div>
       </div>
      </section>
