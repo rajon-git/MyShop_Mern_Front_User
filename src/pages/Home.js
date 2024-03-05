6db048fbd220ba5e4cdfd9dc7a73429e0aa6dc15
rajon-git <rajon.zhsust15@gmail.com>
2024-03-05 11:41:53 +0600

    famous product included

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -200,7 +200,56 @@ function Home() {
       </div>
      </section>
 
-     <section className='featured-wrapper py-5 home-wrapper-2'>
+     <section className='famous-wrapper py-5 home-wrapper-2'>
+      <div className='container-xxl'>
+        <div className='row'>
+          <div className='col-3'>
+            <div className='famous-card position-relative'>
+              <img src='images/famous-01.jpg' className='img-fluid' alt='famous'/>
+              <div className='famous-content position-absolute'>
+                 <h5>Big Screen</h5>
+                 <h6>Smart Watch Series 7</h6>
+                 <p>From $399 or $16.62/mo. for 24 mo.*</p>
+              </div>
+            </div>
+          </div>
+          <div className='col-3'>
+            <div className='famous-card position-relative'>
+              <img src='images/famous-02.jpg' className='img-fluid' alt='famous'/>
+              <div className='famous-content position-absolute'>
+                 <h5 className='text-dark'>Studio Display</h5>
+                 <h6 className='text-dark'>600 nits of brightness</h6>
+                 <p className='text-dark'>27 inch 5k retina Display</p>
+              </div>
+            </div>
+          </div>
+
+          <div className='col-3'>
+            <div className='famous-card position-relative'>
+              <img src='images/famous-03.jpg' className='img-fluid' alt='famous'/>
+              <div className='famous-content position-absolute'>
+                 <h5 className='text-dark'>Smart Phone</h5>
+                 <h6 className='text-dark'>Smartphone 13 pro</h6>
+                 <p className='text-dark'>Now in purple. From $399 or $16.62/mo. for 24 mo.*</p>
+              </div>
+            </div>
+          </div>
+
+          <div className='col-3'>
+            <div className='famous-card position-relative'>
+              <img src='images/famous-04.jpg' className='img-fluid' alt='famous'/>
+              <div className='famous-content position-absolute'>
+                 <h5 className='text-dark'>Home Speakers</h5>
+                 <h6 className='text-dark'>Room Filling Sound</h6>
+                 <p className='text-dark'>Now in purple. From $399 or $16.62/mo. for 24 mo.*</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+     </section>
+
+     <section className='special-wrapper py-5 home-wrapper-2'>
        <div className='container-xxl'>
         <div className='row'>
           <div className='col-12'>
@@ -212,9 +261,29 @@ function Home() {
             <SpecialProduct/>
             <SpecialProduct/>
             <SpecialProduct/>
+            <SpecialProduct/>
           </div>
         </div>
        </div>
+     </section>
+
+     <section className='popular-wrapper py-5 home-wrapper-2'>
+      <div className='container-xxl'>
+        <div className='row'>
+          <div className='col-12'>
+            <h3 className='section-heading'>
+              Our popular products
+            </h3>
+          </div>
+          
+        </div>
+        <div className='row'>
+          <ProductCard/>
+          <ProductCard/>
+          <ProductCard/>
+          <ProductCard/>
+        </div>
+      </div>
      </section>
 
      <section className='marque-wrapper py-5'>
