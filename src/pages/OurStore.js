0a77534ef19d6fbd5f55c163ae3d39aac23015ee
rajon-git <rajon.zhsust15@gmail.com>
2024-03-06 13:04:21 +0600

    our store finding products add

--- src/pages/OurStore.js
+++ src/pages/OurStore.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import BreadCrumb from '../components/BreadCrumb'
 import ReactStars from "react-rating-stars-component";
 import Meta from '../components/Meta';
+import ProductCard from '../components/ProductCard';
 
 function OurStore() {
+    const [grid,setGrid] = useState(4);
+    alert(grid);
     return (
         <>
             <Meta title={"Our Shop"} />
@@ -186,7 +189,56 @@ function OurStore() {
                                 </div>
                             </div>
                         </div>
-                        <div className='col-9'></div>
+                        <div className='col-9'>
+                            <div className='filter-sort-grid mb-4'>
+                               <div className='d-flex justify-content-between align-items-center'>
+                               <div className='d-flex align-items-center gap-10'>
+                                    <p className='mb-0 d-block' style={{"width":"100px"}}>Sort By:</p>
+                                    <select 
+                                       name ='' 
+                                       className='form-control form-select' 
+                                       id=''
+                                    >
+                                       <option value='manual'>Featured</option>
+                                       <option value='best-selling' selected='selected'>Best Selling</option>
+                                       <option value='title-ascending'>Alphabetically, A-Z</option>
+                                       <option value='title-descending'>Alphabetically, Z-A</option>
+                                       <option value='price-ascending'>Price, low to high</option>
+                                       <option value='price-descending'>Price, high to low </option>
+                                       <option value='created-ascending'>Date, old to new</option>
+                                    </select>
+                                </div>
+                                <div className='d-flex align-items-center gap-10'>
+                                    <p className='totalproducts mb-0'>21 products</p>
+                                    <div className='d-flex gap-10 align-items-center grid'>
+                                        <img onClick={()=>{setGrid(4)}}
+                                           src='images/gr4.svg' 
+                                           className='d-block img-fluid' 
+                                           alt='grid'
+                                        />
+                                        <img onClick={()=>{setGrid(3)}}
+                                           src='images/gr3.svg' 
+                                           className='d-block img-fluid' 
+                                           alt='grid'
+                                        />
+                                        <img onClick={()=>{setGrid(2)}}
+                                           src='images/gr2.svg' 
+                                           className='d-block img-fluid' 
+                                           alt='grid'
+                                        />
+                                        <img onClick={()=>{setGrid(1)}}
+                                           src='images/gr.svg' 
+                                           className='d-block img-fluid' 
+                                           alt='grid'
+                                        />
+                                    </div>
+                                </div>
+                               </div>
+                            </div>
+                            <div className='products-list pb-5'>
+                                <ProductCard grid={grid}/>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
