ffbec3beef01122bd65f18308c24c20e3386037b
rajon-git <rajon.zhsust15@gmail.com>
2024-03-07 00:10:45 +0600

    contact page included

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import BreadCrumb from '../components/BreadCrumb'
 import Meta from '../components/Meta';
+import { IoHomeOutline } from "react-icons/io5";
+import { IoCallOutline } from "react-icons/io5";
+import { CiMail } from "react-icons/ci";
+import { IoIosInformationCircleOutline } from "react-icons/io";
 
 function Contact() {
   return (
@@ -21,6 +25,72 @@ function Contact() {
                referrerPolicy="no-referrer-when-downgrade">
 
                </iframe>
+            </div>
+            <div className='col-12 mt-5'>
+              <div className='contact-inner-wrapper d-flex justify-content-between'>
+                <div>
+                <h3 className='contact-title mb-4'>Contact Us</h3>
+                <form action='' className='d-flex flex-column gap-15'>
+                  <div>
+                    <input 
+                      type="text" 
+                      className='form-control'
+                      placeholder='Name'
+                    />
+                  </div>
+                  <div>
+                    <input 
+                      type="email" 
+                      className='form-control'
+                      placeholder='Email'
+                    />
+                  </div>
+                  <div>
+                    <input 
+                      type="tel" 
+                      className='form-control'
+                      placeholder='Mobile Number'
+                    />
+                  </div>
+                  <div>
+                    <textarea  
+                       name="" 
+                       id='' 
+                       className='w-100 form-control'
+                       cols='30' 
+                       rows='4'
+                       placeholder='Comments'
+                    ></textarea>
+                  </div>
+                  <div>
+                    <button className='button border-0'>Submit</button>
+                  </div>
+                </form>
+                </div>
+                <div>
+                <h3 className='contact-title mb-4'>Get in touch with us</h3>
+                <div>
+                  <ul className='ps-0'>
+                    <li className='mb-3 d-flex align-items-center gap-15'>
+                      <IoHomeOutline className='fs-5'/>
+                      <address className='mb-0'>hno: Mirpur-2, Dhaka-1216, pincode:1190282</address>
+                    </li>
+                    <li className='mb-3 d-flex align-items-center gap-15'>
+                      <IoCallOutline className='fs-5'/>
+                      <a href='tel:01990020467'>01990020467</a>
+                    </li>
+                    <li className='mb-3 d-flex align-items-center gap-15'>
+                      <CiMail className='fs-5'/>
+                      <a href='mailto:rajon.zhsust15@gmail.com'>rajon.zhsust15@gmail.com</a>
+                    </li>
+                    <li className='mb-3 d-flex align-items-center gap-15'>
+                      <IoIosInformationCircleOutline className='fs-5'/>
+                      <p className='mb-0'>Saturday-Thursday 10am-8pm</p>
+                    </li>
+                  </ul>
+                </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
