82a9b14c34e1fc938f66ed150ab4949566360413
rajon-git <rajon.zhsust15@gmail.com>
2024-03-06 23:14:33 +0600

    blog filter add

--- src/components/Header.js
+++ src/components/Header.js
@@ -98,7 +98,7 @@ function Header() {
                   <div className='d-flex align-items-center gap-15'>
                     <NavLink to="/">Home</NavLink>
                     <NavLink to="/store">Our Store</NavLink>
-                    <NavLink to="/">Blogs</NavLink>
+                    <NavLink to="/blogs">Blogs</NavLink>
                     <NavLink to="/contact">Contact</NavLink>
                   </div>
                 </div>
