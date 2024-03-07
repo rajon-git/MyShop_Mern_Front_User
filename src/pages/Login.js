cb363dff4b79a909257f13e5f96b27d5f8450c65
rajon-git <rajon.zhsust15@gmail.com>
2024-03-07 13:12:41 +0600

    login page added

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import BreadCrumb from '../components/BreadCrumb'
 import Meta from '../components/Meta';
+import { Link } from 'react-router-dom';
 
 function Login() {
   return (
@@ -10,9 +11,23 @@ function Login() {
       <div className='login-wrapper home-wrapper-2 py-5'>
         <div className='row'>
             <div className='col-12'>
-                <div className='login-card'>
-                    <h3>Login</h3>
-                    <form action=''></form>
+                <div className='auth-card'>
+                    <h3 className='text-center mb-3'>Login</h3>
+                    <form action='' className='d-flex flex-column gap-15'>
+                      <div>
+                        <input type='email' placeholder='email' name='email' className='form-control'/>
+                      </div>
+                      <div className='mt-1'>
+                        <input type='password' placeholder='password' name='password' className='form-control'/>
+                      </div>
+                      <div>
+                        <Link to='/forgot-password'>Forgot Password</Link>
+                        <div className='mt-3 d-flex justify-content-center align-items-center gap-20'>
+                          <button className='button border-0'>Login</button>
+                          <Link className='button signup'>Signup</Link>
+                        </div>
+                      </div>
+                    </form>
                 </div>
             </div>
         </div>
