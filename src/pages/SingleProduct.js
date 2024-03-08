f020e7db9c18949ff912772218ebc8b49a3b2193
rajon-git <rajon.zhsust15@gmail.com>
2024-03-09 01:19:57 +0600

    product images add

--- src/pages/SingleProduct.js
+++ src/pages/SingleProduct.js
@@ -3,8 +3,15 @@ import BreadCrumb from "../components/BreadCrumb";
 import Meta from "../components/Meta";
 import ProductCard from "../components/ProductCard";
 import ReactStars from "react-rating-stars-component";
+import ReactImageZoom from "react-image-zoom";
 
 function SingleProduct() {
+    const props = {
+        width: 400,
+        height: 500, 
+        zoomWidth: 500,
+        img: "https://ph-live-01.slatic.net/p/b06c1fbd4b655af3fce96b10ff66026d.jpg",
+      };
   const [orderedProduct, setOrderedProduct] = useState(true);
   return (
     <>
@@ -13,7 +20,27 @@ function SingleProduct() {
       <div className="main-product-wrapper home-wrapper-2 py-5">
         <div className="container-xxl">
           <div className="row">
-            <div className="col-6"></div>
+            <div className="col-6">
+              <div className="main-product-image">
+                <div>
+                  <ReactImageZoom {...props} />
+                </div>
+              </div>
+              <div className="other-product-images d-flex flex-wrap gap-15">
+                <div>
+                    <img src="https://ph-live-01.slatic.net/p/b06c1fbd4b655af3fce96b10ff66026d.jpg" className="img-fluid" alt=""/>
+                </div>
+                <div>
+                   <img src="https://ph-live-01.slatic.net/p/b06c1fbd4b655af3fce96b10ff66026d.jpg" className="img-fluid" alt=""/>
+                </div>
+                <div>
+                   <img src="https://ph-live-01.slatic.net/p/b06c1fbd4b655af3fce96b10ff66026d.jpg" className="img-fluid" alt=""/>
+                </div>
+                <div>
+                   <img src="https://ph-live-01.slatic.net/p/b06c1fbd4b655af3fce96b10ff66026d.jpg" className="img-fluid" alt=""/>
+                </div>
+              </div>
+            </div>
             <div className="col-6"></div>
           </div>
         </div>
@@ -22,9 +49,8 @@ function SingleProduct() {
         <div className="container-xxl">
           <div className="row">
             <div className="col-12">
-            <h4>Description</h4>
+              <h4>Description</h4>
               <div className="bg-white p-3">
-                
                 <p>
                   Lorem Ipsum is simply dummy text of the printing and
                   typesetting industry. Lorem Ipsum has been the industry's
@@ -46,72 +72,80 @@ function SingleProduct() {
         <div className="container-xxl">
           <div className="row">
             <div className="col-12">
-            <h3>Reviews</h3>
+              <h3>Reviews</h3>
               <div className="review-inner-wrapper">
-              <div className="review-head d-flex justify-content-between align-items-end">
-                <div>
-                  <h4 className="mb-2">Customer Reviews</h4>
-                  <div className="d-flex align-items-center gap-10">
-                    <ReactStars
-                      count={5}
-                      size={24}
-                      value={4}
-                      edit={false}
-                      activeColor="#ffd700"
-                    />
-                    <p className="mb-0">Based on 2 reviews</p>
-                  </div>
-                </div>
-                {orderedProduct && (
+                <div className="review-head d-flex justify-content-between align-items-end">
                   <div>
-                    <a className="text-dark text-decoration-underline" href="">
-                      Write a review
-                    </a>
-                  </div>
-                )}
-              </div>
-              <div className="review-form py-4">
-                <h4> Write a review</h4>
-              <form action='' className='d-flex flex-column gap-15'>
-                  <div>
-                  <ReactStars
-                      count={5}
-                      size={24}
-                      value={4}
-                      edit={true}
-                      activeColor="#ffd700"
-                    />
-                  </div>
-                  <div>
-                    <textarea  
-                       name="" 
-                       id='' 
-                       className='w-100 form-control'
-                       cols='30' 
-                       rows='4'
-                       placeholder='Comments'
-                    ></textarea>
-                  </div>
-                  <div className="d-flex justify-content-end">
-                    <button className='button border-0'>Submit Review</button>
-                  </div>
-                </form>
-              </div>
-              <div className="reviews mt-4">
-                <div className="review">
-                <div className="d-flex gap-10 align-items-center">
-                    <h6 className="mb-0">Rajon</h6>
-                    <ReactStars
+                    <h4 className="mb-2">Customer Reviews</h4>
+                    <div className="d-flex align-items-center gap-10">
+                      <ReactStars
                         count={5}
                         size={24}
                         value={4}
                         edit={false}
                         activeColor="#ffd700"
-                    />
+                      />
+                      <p className="mb-0">Based on 2 reviews</p>
+                    </div>
+                  </div>
+                  {orderedProduct && (
+                    <div>
+                      <a
+                        className="text-dark text-decoration-underline"
+                        href=""
+                      >
+                        Write a review
+                      </a>
+                    </div>
+                  )}
                 </div>
-                    <p className="mt-3">It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                <div className="review-form py-4">
+                  <h4> Write a review</h4>
+                  <form action="" className="d-flex flex-column gap-15">
+                    <div>
+                      <ReactStars
+                        count={5}
+                        size={24}
+                        value={4}
+                        edit={true}
+                        activeColor="#ffd700"
+                      />
+                    </div>
+                    <div>
+                      <textarea
+                        name=""
+                        id=""
+                        className="w-100 form-control"
+                        cols="30"
+                        rows="4"
+                        placeholder="Comments"
+                      ></textarea>
+                    </div>
+                    <div className="d-flex justify-content-end">
+                      <button className="button border-0">Submit Review</button>
+                    </div>
+                  </form>
                 </div>
-              </div>
+                <div className="reviews mt-4">
+                  <div className="review">
+                    <div className="d-flex gap-10 align-items-center">
+                      <h6 className="mb-0">Rajon</h6>
+                      <ReactStars
+                        count={5}
+                        size={24}
+                        value={4}
+                        edit={false}
+                        activeColor="#ffd700"
+                      />
+                    </div>
+                    <p className="mt-3">
+                      It was popularised in the 1960s with the release of
+                      Letraset sheets containing Lorem Ipsum passages, and more
+                      recently with desktop publishing software like Aldus
+                      PageMaker including versions of Lorem Ipsum.
+                    </p>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
