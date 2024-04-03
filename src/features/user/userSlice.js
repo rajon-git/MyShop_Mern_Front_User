f029d594f368a2c6ab8394f19f522cda8d3b6b61
rajon-git <rajon.zhsust15@gmail.com>
2024-04-03 20:05:30 +0600

    user login slice created

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -13,6 +13,17 @@ export const registerUser = createAsyncThunk(
   }
 );
 
+export const loginUser = createAsyncThunk(
+  "auth/login",
+  async (userData, thunkAPI) => {
+    try {
+      return await authService.login(userData);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+
 const initialState = {
   user: "",
   isError: false,
@@ -46,7 +57,29 @@ export const authSlice = createSlice({
         state.isSuccess = false;
         state.message = action.error;
         if (state.isError === true) {
-          toast.info("action.error");
+          toast.error(action.error);
+        }
+      })
+      .addCase(loginUser.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(loginUser.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isError = false;
+        state.isSuccess = true;
+        state.user = action.payload;
+        state.message = "success";
+        if (state.isSuccess === true) {
+          toast.info("User logged in");
+        }
+      })
+      .addCase(loginUser.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.isSuccess = false;
+        state.message = action.error;
+        if (state.isError === true) {
+          toast.error(action.error);
         }
       });
   },
