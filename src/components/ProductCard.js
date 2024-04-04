68d27d96e3a48e95f66b85ffc9c273660d7a3c69
rajon-git <rajon.zhsust15@gmail.com>
2024-04-05 01:16:33 +0600

    our store products show complete

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,131 +1,93 @@
 import React from "react";
 import ReactStars from "react-rating-stars-component";
 import { Link, useLocation } from "react-router-dom";
-import prodcompare from "../images/prodcompare.svg"
-import wish from "../images/wish.svg"
-import wishlist from "../images/wishlist.svg"
-import watch from"../images/watch.jpg"
-import tab1 from "../images/tab1.jpg"
-import addcart from "../images/add-cart.svg"
-import view from "../images/view.svg"
+import prodcompare from "../images/prodcompare.svg";
+import wish from "../images/wish.svg";
+import wishlist from "../images/wishlist.svg";
+import watch from "../images/watch.jpg";
+import tab1 from "../images/tab1.jpg";
+import addcart from "../images/add-cart.svg";
+import view from "../images/view.svg";
 
 function ProductCard(props) {
-  const { grid } = props;
+  const { grid, data } = props;
   let location = useLocation();
+  console.log(data);
   return (
     <>
-      <div
-        className={`${location.pathname === "/product" ? `gr-${grid}` : "col-3"}`}
-      >
-        <Link to={`${location.pathname == "/" ? '/product/:id' : location.pathname == '/product/:id' ? '/product/1':":id"}`} className="product-card position-relative">
-          <div className="wishlist-icon position-absolute">
-            <button className="border-0 bg-transparent">
-              <img src={wish} alt="wish" />
-            </button>
+      {data?.map((item, i) => {
+        return (
+          <div
+            className={`${
+              location.pathname === "/product" ? `gr-${grid}` : "col-3"
+            }`}
+          >
+            <Link
+              to={`${
+                location.pathname === "/"
+                  ? "/product/:id"
+                  : location.pathname === "/product/:id"
+                  ? "/product/1"
+                  : ":id"
+              }`}
+              className="product-card position-relative"
+            >
+              <div className="wishlist-icon position-absolute">
+                <button className="border-0 bg-transparent">
+                  <img src={wish} alt="wish" />
+                </button>
+              </div>
+              <div className="product-image">
+                <img
+                  src={item?.images[0].url}
+                  alt="product image"
+                  className="img-fluid  mx-auto"
+                  width={160}
+                />
+                <img
+                  src={tab1}
+                  alt="product image"
+                  className="img-fluid  mx-auto"
+                  width={160}
+                />
+              </div>
+              <div className="product-details">
+                <h6 className="brand">{item?.brand}</h6>
+                <h5 className="product-title">{item?.title}</h5>
+                <ReactStars
+                  count={5}
+                  size={24}
+                  value={item?.totalrating.toString()}
+                  edit={false}
+                  activeColor="#ffd700"
+                />
+                <p
+                  className={`description ${
+                    grid === 12 ? "d-block" : "d-none"
+                  }`}
+                >
+                  {item?.description}
+                  {/* dangerouslySetInnerHTML={{__html: item?.description}} */}
+                </p>
+                <p className="price">{item?.price}</p>
+              </div>
+              <div className="action-bar position-absolute">
+                <div className="d-flex flex-column gap-15">
+                  <button className="border-0 bg-transparent">
+                    <img src={prodcompare} alt="prodcompare" />
+                  </button>
+                  <button className="border-0 bg-transparent">
+                    <img src={view} alt="view" />
+                  </button>
+                  <button className="border-0 bg-transparent">
+                    <img src={addcart} alt="add-cart" />
+                  </button>
+                </div>
+              </div>
+            </Link>
           </div>
-          <div className="product-image">
-            <img
-              src={watch}
-              alt="product image"
-              className="img-fluid"
-            />
-            <img
-              src={tab1}
-              alt="product image"
-              className="img-fluid"
-            />
-          </div>
-          <div className="product-details">
-            <h6 className="brand">Havels</h6>
-            <h5 className="product-title">
-              Kids Headphone Bult 10 pack multi colored studesnt
-            </h5>
-            <ReactStars
-              count={5}
-              size={24}
-              value={4}
-              edit={false}
-              activeColor="#ffd700"
-            />
-            <p className={`description ${grid === 12 ? "d-block" : "d-none"}`}>
-              Get started with Bootstrap, the world's most popular framework for
-              building responsive, mobile-first sites, with jsDelivr and a
-              template starter page.
-            </p>
-            <p className="price">$100.00</p>
-          </div>
-          <div className="action-bar position-absolute">
-            <div className="d-flex flex-column gap-15">
-              <button className="border-0 bg-transparent">
-                <img src={prodcompare} alt="prodcompare" />
-              </button >
-              <button className="border-0 bg-transparent">
-                <img src={view} alt="view" />
-              </button >
-              <button className="border-0 bg-transparent">
-                <img src={addcart} alt="add-cart" />
-              </button >
-            </div>
-          </div>
-        </Link>
-      </div>
-      <div
-        className={`${
-          location.pathname === "/product" ? `gr-${grid}` : "col-3"
-        }`}
-      >
-        <Link to={`${location.pathname == '/' ? 'product/:id':":id"}`} className="product-card position-relative">
-          <div className="wishlist-icon position-absolute">
-            <button className="border-0 bg-transparent">
-              <img src={wish} alt="wishlist" />
-            </button>
-          </div>
-          <div className="product-image">
-            <img
-              src={watch}
-              alt="product image"
-              className="img-fluid"
-            />
-            <img
-              src={tab1}
-              alt="product image"
-              className="img-fluid"
-            />
-          </div>
-          <div className="product-details">
-            <h6 className="brand">Havels</h6>
-            <h5 className="product-title">
-              Kids Headphone Bult 10 pack multi colored studesnt
-            </h5>
-            <ReactStars
-              count={5}
-              size={24}
-              value={4}
-              edit={false}
-              activeColor="#ffd700"
-            />
-            <p className={`description ${grid === 12 ? "d-block" : "d-none"}`}>
-              Get started with Bootstrap, the world's most popular framework for
-              building responsive, mobile-first sites, with jsDelivr and a
-              template starter page.
-            </p>
-            <p className="price">$100.00</p>
-          </div>
-          <div className="action-bar position-absolute">
-            <div className="d-flex flex-column gap-15">
-              <button className="border-0 bg-transparent">
-                <img src={prodcompare} alt="prodcompare" />
-              </button>
-              <button className="border-0 bg-transparent">
-                <img src={view} alt="view" />
-              </button>
-              <button className="border-0 bg-transparent">
-                <img src={addcart} alt="add-cart" />
-              </button>
-            </div>
-          </div>
-        </Link>
-      </div>
+        );
+      })}
     </>
   );
 }
--- src/features/products/productService.js
+++ src/features/products/productService.js
@@ -2,8 +2,7 @@ import axios from "axios"
 import { base_url } from "../../utils/axiosConfig";
 
 const getProducts = async()=>{
-    const response = await axios.get(`${base_url}product`);
-
+    const response = await axios.get(`${base_url}product/`);
     if(response.data)
     {
         return response.data;
