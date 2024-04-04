e6965aa785c96ec30da0ea17005546a6ec5b7e48
rajon-git <rajon.zhsust15@gmail.com>
2024-04-04 12:24:25 +0600

    header and footer make responsive

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -32,7 +32,7 @@ function Footer() {
       </div>
      </footer>
 
-     <footer className='py-3'>
+     {/* <footer className='py-3'>
        <div className='container-xxl'>
         <div className='row'>
           <div className='col-4'>
@@ -94,7 +94,66 @@ function Footer() {
           </div>
         </div>
        </div>
-     </footer>
+     </footer> */}
+
+<footer className='py-5 text-white'>
+  <div className='container-xl'>
+    <div className='row'>
+      <div className='col-lg-4 col-md-6 col-sm-12 mb-4'>
+        <h4 className='mb-4'>About Us</h4>
+        <p className='fs-6'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.</p>
+        <address className='text-muted fs-6 mt-3'>
+          hno: Mirpur-2,<br/>
+          Dhaka-1216<br/>
+          pincode:1190282
+        </address>
+        <div className='social-icons mt-4'>
+          <a href='#' className='text-white me-3'>
+            <BsLinkedin className='fs-4'/>
+          </a>
+          <a href='#' className='text-white me-3'>
+             <BsGithub className='fs-4'/>
+          </a>
+          <a href='#' className='text-white me-3'>
+            <BsYoutube className='fs-4'/>
+          </a>
+          <a href='#' className='text-white'>
+             <BsInstagram className='fs-4'/>
+          </a>
+        </div>
+      </div>
+      <div className='col-lg-3 col-md-6 col-sm-12 mb-4'>
+         <h4 className='mb-4'>Information</h4>
+         <ul className='list-unstyled'>
+          <li><Link to='/privacy-policy' className='text-muted mb-2 text-decoration-none'>Privacy Policy</Link></li>
+          <li><Link to='/refund-policy' className='text-muted mb-2 text-decoration-none'>Refund Policy</Link></li>
+          <li><Link to='/shipping-policy' className='text-muted mb-2 text-decoration-none'>Shipping Policy</Link></li>
+          <li><Link to='/terms-and-condition' className='text-muted mb-2 text-decoration-none'>Terms & Condition</Link></li>
+          <li><Link to='/blogs' className='text-muted mb-2 text-decoration-none'>Blogs</Link></li>
+        </ul>
+      </div>
+      <div className='col-lg-3 col-md-6 col-sm-12 mb-4'>
+        <h4 className='mb-4'>Contact</h4>
+        <ul className='list-unstyled'>
+          <li><Link className='text-muted mb-2 text-decoration-none'>About us</Link></li>
+          <li><Link className='text-muted mb-2 text-decoration-none'>Faq</Link></li>
+          <li><Link className='text-muted mb-2 text-decoration-none'>Contact</Link></li>
+        </ul>
+      </div>
+      <div className='col-lg-2 col-md-6 col-sm-12'>
+        <h4 className='mb-4'>Quick Links</h4>
+        <ul className='list-unstyled'>
+          <li><Link className='text-muted mb-2 text-decoration-none'>Laptops</Link></li>
+          <li><Link className='text-muted mb-2 text-decoration-none'>Headphones</Link></li>
+          <li><Link className='text-muted mb-2 text-decoration-none'>Tablet</Link></li>
+          <li><Link className='text-muted mb-2 text-decoration-none'>Watch</Link></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</footer>
+
+
 
      <footer className='py-4'>
       <div className='container-xxl'>
--- src/components/Header.js
+++ src/components/Header.js
@@ -20,7 +20,7 @@ function Header() {
         </div>
       </header>
 
-      <header className='header-upper py-3'>
+      {/* <header className='header-upper py-3'>
         <div className='container-xxl'>
           <div className='row align-items-center'>
             <div className='col-2'>
@@ -74,17 +74,74 @@ function Header() {
             </div>
           </div>
         </div>
+      </header> */}
+
+      <header className='header-upper py-3'>
+        <div className='container-xxl'>
+          <div className='row align-items-center'>
+            <div className='col-lg-2 col-md-3 col-6'>
+              <h2>
+                <Link className='text-white'>Developers</Link>
+              </h2>
+            </div>
+            <div className='col-lg-5 col-md-6 col-6 mt-3 mt-md-0'>
+              <div className="input-group">
+                <input
+                  type="text"
+                  className="form-control py-2"
+                  placeholder="Search Product Here"
+                  aria-label="Search Product Here"
+                  aria-describedby="basic-addon2" />
+                <span className="input-group-text p-3" id="basic-addon2">
+                  <IoIosSearch className='fs-6 search-icon' />
+                </span>
+              </div>
+            </div>
+            <div className='col-lg-5 col-md-3 col-12 mt-3 mt-md-0'>
+              <div className='header-upper-links d-flex align-items-center justify-content-between'>
+                <div>
+                  <Link to='/compare-product' className='d-flex align-items-center gap-10 text-white'>
+                    <img src='images/compare.svg' alt='compare' />
+                    <p className='mb-0'>Compare</p>
+                  </Link>
+                </div>
+                <div>
+                  <Link to='/wishlist' className='d-flex align-items-center gap-10 text-white'>
+                    <img src='images/wishlist.svg' alt='wishlist' />
+                    <p className='mb-0'>Wishlist</p>
+                  </Link>
+                </div>
+                <div>
+                  <Link to='/login' className='d-flex align-items-center gap-10 text-white'>
+                    <img src='images/user.svg' alt='user' />
+                    <p className='mb-0'>Login</p>
+                  </Link>
+                </div>
+                <div>
+                  <Link to="/cart" className='d-flex align-items-center gap-10 text-white'>
+                    <img src='images/cart.svg' alt='cart' />
+                    <div className='d-flex flex-column gap-10'>
+                      <span className='badge bg-white text-dark'>0</span>
+                      <p className='mb-0'>$ 500</p>
+                    </div>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </header>
+
 
       <header className='header-bottom py-3'>
         <div className='container-xxl'>
           <div className='row'>
             <div className='col-12'>
-              <div className='menu-bottom d-flex align-items-center gap-30'>
+              <div className='menu-bottom d-flex align-items-center gap-lg-30 gap-10'>
                 <div>
                   <div className="dropdown">
-                    <button className="btn btn-secondary dropdown-toggle bg-transparent border-0 gap-15 d-flex align-items-center" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-                      <img src='images/menu.svg' alt=''/>
+                    <button className="btn btn-secondary dropdown-toggle bg-transparent border-0 gap-lg-15 gap-25 d-flex align-items-center" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                      <img src='images/menu.svg' alt='' />
                       <span className='me-5 d-inline-block'>Shop Categories</span>
                     </button>
                     <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
