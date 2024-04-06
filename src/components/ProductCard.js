ec3638559ab4df6edff7efd7cb984eda36dbbd05
rajon-git <rajon.zhsust15@gmail.com>
2024-04-06 23:59:37 +0600

    one error fixed

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -67,7 +67,7 @@ function ProductCard(props) {
                   size={24}
                   value={item?.totalrating.toString()}
                   edit={false}
-                  activeColor=
+                  activeColor="#ffd700"
                 />
                 <p
                   className={`description ${
