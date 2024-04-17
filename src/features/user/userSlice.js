b497265dbc8b4ff30d5c32c7f03a74293c7fd8e3
rajon-git <rajon.zhsust15@gmail.com>
2024-04-17 22:01:58 +0600

    wishlist added but face some error

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -25,7 +25,7 @@ export const loginUser = createAsyncThunk(
 );
 
 export const getUserProductWishlist = createAsyncThunk(
-  "auth/wishlist",
+  "user/wishlist",
   async (thunkAPI) => {
     try {
       return await authService.getUserWishlist();
--- src/pages/Wishlist.js
+++ src/pages/Wishlist.js
@@ -1,57 +1,55 @@
-import React from 'react'
-import BreadCrumb from '../components/BreadCrumb'
-import Meta from '../components/Meta';
-import Container from '../components/Container';
+import React, { useEffect } from "react";
+import BreadCrumb from "../components/BreadCrumb";
+import Meta from "../components/Meta";
+import Container from "../components/Container";
+import { useDispatch, useSelector } from "react-redux";
+import { getUserProductWishlist } from "../features/user/userSlice";
 
 function Wishlist() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    getWislistfromdb();
+  }, []);
+  const getWislistfromdb = () => {
+    dispatch(getUserProductWishlist());
+  };
+  const wishlistState = useSelector((state) => state.auth.wishlist);
   return (
     <>
-     <Meta title={"Wishlist"} />
-      <BreadCrumb title='Wishlist' />
-      <Container class1='wishlist-wrapper home-wrapper-2 py-5'>
-       
-            <div className='row'>
-                <div className='col-3'>
-                    <div className='wishlist-card position-relative'>
-                    <img src='images/cross.svg' alt='cross' className='position-absolute cross img-fluid'/>
-                        <div className='wishlist-card-image'>
-                            <img src='images/watch.jpg' className='img-fluid w-100' alt='watch'/>
-                        </div>
-                        <div className='py-3 px-3'>
-                        <h5 className='title'>Honor t1 7.0 1gb ram 8gb rom 7 inch width wi-fi+3g tablet</h5>
-                        <h6 className='price'>$ 200</h6>
-                        </div>
-                    </div>
+      <Meta title={"Wishlist"} />
+      <BreadCrumb title="Wishlist" />
+      <Container class1="wishlist-wrapper home-wrapper-2 py-5">
+        <div className="row">
+          {wishlistState?.map((item, i) => {
+            return (
+              <div className="col-3" key={i}>
+                <div className="wishlist-card position-relative">
+                  <img
+                    src={item?.image[0].url ? item?.image[0].url : "images/cross.svg"}
+                    alt="cross"
+                    className="position-absolute cross img-fluid"
+                  />
+                  <div className="wishlist-card-image">
+                    <img
+                      src={item?.image[0].url ? item?.image[0].url : "images/watch.jpg"}
+                      className="img-fluid w-100"
+                      alt="watch"
+                    />
+                  </div>
+                  <div className="py-3 px-3">
+                    <h5 className="title">
+                      {item?.title}
+                    </h5>
+                    <h6 className="price">$ {item?.price}</h6>
+                  </div>
                 </div>
-                <div className='col-3'>
-                    <div className='wishlist-card position-relative'>
-                    <img src='images/cross.svg' alt='cross' className='position-absolute cross img-fluid'/>
-                        <div className='wishlist-card-image'>
-                            <img src='images/watch.jpg' className='img-fluid w-100' alt='watch'/>
-                        </div>
-                        <div className='py-3 px-3'>
-                        <h5 className='title'>Honor t1 7.0 1gb ram 8gb rom 7 inch width wi-fi+3g tablet</h5>
-                        <h6 className='price'>$ 200</h6>
-                        </div>
-                    </div>
-                </div>
-                <div className='col-3'>
-                    <div className='wishlist-card position-relative'>
-                    <img src='images/cross.svg' alt='cross' className='position-absolute cross img-fluid'/>
-                        <div className='wishlist-card-image'>
-                            <img src='images/watch.jpg' className='img-fluid w-100' alt='watch'/>
-                        </div>
-                        <div className='py-3 px-3'>
-                        <h5 className='title'>Honor t1 7.0 1gb ram 8gb rom 7 inch width wi-fi+3g tablet</h5>
-                        <h6 className='price'>$ 200</h6>
-                        </div>
-                    </div>
-                </div>
-            </div>
-       
+              </div>
+            );
+          })}
+        </div>
       </Container>
     </>
-  )
+  );
 }
 
-export default Wishlist
+export default Wishlist;
