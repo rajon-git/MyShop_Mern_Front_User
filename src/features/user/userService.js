b821b622dce101692aad8a8826f131068a2bcca3
rajon-git <rajon.zhsust15@gmail.com>
2024-04-17 21:34:45 +0600

    get user wishlist service add

--- src/features/user/userService.js
+++ src/features/user/userService.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-import { base_url } from "../../utils/axiosConfig";
+import { base_url, config } from "../../utils/axiosConfig";
 
 const register = async(userData)=>{
     const response = await axios.post(`${base_url}user/register`, userData);
@@ -19,7 +19,16 @@ const login = async(userData)=>{
     }
 }
 
+const getUserWishlist = async()=>{
+    const response = await axios.get(`${base_url}user/wishlist`, config)
+    if(response.data)
+    {
+        return response.data;
+    }
+}
+
 export const authService = {
     register,
-    login
+    login,
+    getUserWishlist
 }
