da77721d023ce3c9f3b4681a3a45077cd8cafa71
rajon-git <rajon.zhsust15@gmail.com>
2024-04-19 14:01:44 +0600

    get single block service add

--- src/features/blogs/blogService.js
+++ src/features/blogs/blogService.js
@@ -9,7 +9,16 @@ const getBlogs = async()=>{
     }
 }
 
+const getBlog = async(id)=>{
+    const response = await axios.get(`${base_url}blog/${id}`);
+    if(response.data)
+    {
+        return response.data;
+    }
+}
+
 
 export const blogService = {
-    getBlogs
+    getBlogs,
+    getBlog
 }
