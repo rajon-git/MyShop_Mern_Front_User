48f099ad05f02716399e5a8655f38ab64e0f4be6
rajon-git <rajon.zhsust15@gmail.com>
2024-04-23 00:33:38 +0600

    update in wishlist

--- src/pages/Wishlist.js
+++ src/pages/Wishlist.js
@@ -28,7 +28,9 @@ function Wishlist() {
       <Container class1="wishlist-wrapper home-wrapper-2 py-5">
         <div className="row">
           {
-            wishlistState.length === 0 && <div className="text-center fs-3">No Data</div>
+            wishlistState && wishlistState.length === 0 && (
+              <div className="text-center fs-3">No Data</div>
+            )
           }
           {wishlistState?.map((item, index) => {
             return (
