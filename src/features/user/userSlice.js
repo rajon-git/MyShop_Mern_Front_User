bfa557c24abb0c17cc5802d9614341c002224040
rajon-git <rajon.zhsust15@gmail.com>
2024-04-23 21:39:46 +0600

    get cart slice add

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -46,6 +46,17 @@ export const addProdToCart = createAsyncThunk(
   }
 );
 
+export const getUserCart = createAsyncThunk(
+  "user/cart/get",
+  async (thunkAPI) => {
+    try {
+      return await authService.getCart();
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+
 const token = localStorage.getItem("customer")
   ? JSON.parse(localStorage.getItem("customer"))
   : null;
@@ -133,6 +144,21 @@ export const authSlice = createSlice({
         state.cartProduct = action.payload;
       })
       .addCase(addProdToCart.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.isSuccess = false;
+        state.message = action.error;
+      })
+      .addCase(getUserCart.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getUserCart.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isError = false;
+        state.isSuccess = true;
+        state.cartProducts = action.payload;
+      })
+      .addCase(getUserCart.rejected, (state, action) => {
         state.isLoading = false;
         state.isError = true;
         state.isSuccess = false;
