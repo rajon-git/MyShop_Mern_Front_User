0edbb72a3dc0c243b2e2671e77f8680083f95f34
rajon-git <rajon.zhsust15@gmail.com>
2024-04-28 00:18:02 +0600

    orders ui implemented

--- src/pages/Orders.js
+++ src/pages/Orders.js
@@ -1,75 +1,104 @@
-import React, { useEffect } from 'react'
-import BreadCrumb from '../components/BreadCrumb'
-import Container from '../components/Container'
-import { useDispatch, useSelector } from 'react-redux'
-import { getOrders } from '../features/user/userSlice';
-
+import React, { useEffect } from "react";
+import BreadCrumb from "../components/BreadCrumb";
+import Container from "../components/Container";
+import { useDispatch, useSelector } from "react-redux";
+import { getOrders } from "../features/user/userSlice";
 
 function Orders() {
-    const dispatch = useDispatch();
-    const orderState = useSelector((state)=> state?.auth?.getOrderedProduct);
+  const dispatch = useDispatch();
+  const orderState = useSelector(
+    (state) => state?.auth?.getOrderedProduct?.orders
+  );
 
-    useEffect(()=>{
-        dispatch(getOrders());
-    },[])
+  useEffect(() => {
+    dispatch(getOrders());
+  }, []);
   return (
     <>
-    <BreadCrumb title='My Orders'/>
-      <Container class1='cart-wrapper home-wrapper-2 py-5'>
-        <div className='row'>
-            <div className='col-12'>
-                <div className='row'>
-                    <div className='col-3'>
-                        <h5>Order Id</h5>
-                    </div>
-                    <div className='col-3'>
-                        <h5>Total Amount</h5>
-                    </div>
-                    <div className='col-3'>
-                        <h5>Total Amount After Discount</h5>
-                    </div>
-                    <div className='col-3'>
-                        <h5>Status</h5>
-                    </div>
-                </div>
-                
+      <BreadCrumb title="My Orders" />
+      <Container class1="cart-wrapper home-wrapper-2 py-5">
+        <div className="row">
+          <div className="col-12">
+            <div className="row">
+              <div className="col-3">
+                <h5>Order Id</h5>
+              </div>
+              <div className="col-3">
+                <h5>Total Amount</h5>
+              </div>
+              <div className="col-3">
+                <h5>Total Amount After Discount</h5>
+              </div>
+              <div className="col-3">
+                <h5>Status</h5>
+              </div>
             </div>
-            <div className='col-12 mt-3'>
-            <div className='row'>
-                    <div className='col-3'>
-                        <p>Order Id</p>
+          </div>
+          <div className="col-12 mt-3">
+            {orderState &&
+              orderState?.map((item, index) => {
+                return (
+                  <div style={{backgroundColor: "#febd69"}} className="row pt-3 my-3" key={index}>
+                    <div className="col-3">
+                      <p>{item?._id}</p>
                     </div>
-                    <div className='col-3'>
-                        <p>Total Amount</p>
+                    <div className="col-3">
+                      <p>{item?.totalPrice}</p>
                     </div>
-                    <div className='col-3'>
-                        <p>Total Amount After Discount</p>
+                    <div className="col-3">
+                      <p>{item?.totalPriceAfterDiscount}</p>
                     </div>
-                    <div className='col-3'>
-                        <p>Status</p>
+                    <div className="col-3">
+                      <p>{item?.orderStatus}</p>
                     </div>
-                    <div className='col-12'>
-                        <div className='row bg-secondary p-3'>
-                        <div className='col-3'>
-                        <p>Order Id</p>
-                    </div>
-                    <div className='col-3'>
-                        <p>Total Amount</p>
-                    </div>
-                    <div className='col-3'>
-                        <p>Total Amount After Discount</p>
-                    </div>
-                    <div className='col-3'>
-                        <p>Status</p>
-                    </div>
+                    <div className="col-12">
+                      <div className="row p-3" style={{backgroundClip: "#232f3e"}}>
+                        <div className="col-3">
+                          <h6>Product Name</h6>
                         </div>
+                        <div className="col-3">
+                          <h6>Quantity</h6>
+                        </div>
+                        <div className="col-3">
+                          <h6>Price</h6>
+                        </div>
+                        <div className="col-3">
+                          <h6>Color</h6>
+                        </div>
+                        {item?.orderItems?.map((i, index) => {
+                          return (
+                            <div className="col-12">
+                              <div className="row bg-secondary p-3">
+                                <div className="col-3">
+                                  <p>{i?.product?.title}</p>
+                                </div>
+                                <div className="col-3">
+                                  <p>{i?.quantity}</p>
+                                </div>
+                                <div className="col-3">
+                                  <p>{i?.price}</p>
+                                </div>
+                                <div className="col-3">
+                                  <ul className="colors ps-0">
+                                    <li
+                                      style={{ backgroundColor: i?.color }}
+                                    ></li>
+                                  </ul>
+                                </div>
+                              </div>
+                            </div>
+                          );
+                        })}
+                      </div>
                     </div>
-                </div>
-            </div>
+                  </div>
+                );
+              })}
+          </div>
         </div>
       </Container>
     </>
-  )
+  );
 }
 
-export default Orders
+export default Orders;
