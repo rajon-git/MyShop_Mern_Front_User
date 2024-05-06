e2f032541ed9fc3825fef2b48922d390948cd22d
rajon-git <rajon.zhsust15@gmail.com>
2024-05-06 23:47:03 +0600

    special product responsive

--- src/components/SpecialProduct.js
+++ src/components/SpecialProduct.js
@@ -5,11 +5,11 @@ import { Link } from 'react-router-dom';
 function SpecialProduct(props) {
     const {id,title, brand,image, totalrating, price, sold, quantity} = props;
     return (
-        <div className='col-6 mb-3'>
+        <div className='col-lg-6 col-md-6 col-sm-12 col-12 mb-3'>
             <div className='special-product-card'>
                 <div className='d-flex justify-content-between'>
                     <div>
-                        <img style={{width:300, height:300}} src={image ? image : 'images/watch.jpg'} alt={image ? image.name : 'watch'} className='img-fluid' />
+                        <img style={{width:200, height:300}} src={image ? image : 'images/watch.jpg'} alt={image ? image.name : 'watch'} className='img-fluid' />
                     </div>
                     <div className='special-product-content'>
                         <h5 className='brand'>{brand}</h5>
@@ -27,7 +27,7 @@ function SpecialProduct(props) {
                             <span className='red-p'>
                                 {price}
                             </span> &nbsp;
-                            <strike>$200</strike>
+                            <strike>1200</strike>
                         </p>
                         <div className='discout-till d-flex align-items-center gap-10'>
                             <p className='mb-0'>
