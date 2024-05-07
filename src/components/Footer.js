7d2ce414a801868dd6cd77d67a3c24e53863dfaa
rajon-git <rajon.zhsust15@gmail.com>
2024-05-07 21:36:13 +0600

    footer section make responsive

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,38 +1,39 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { BsLinkedin, BsGithub, BsYoutube , BsInstagram   } from "react-icons/bs";
-import newsletter from "../images/newsletter.png"
+import React from "react";
+import { Link } from "react-router-dom";
+import { BsLinkedin, BsGithub, BsYoutube, BsInstagram } from "react-icons/bs";
+import newsletter from "../images/newsletter.png";
 
 function Footer() {
   return (
-    <> 
-     <footer className='py-4'>
-      <div className='container-xxl'>
-        <div className='row align-items-center'>
-          <div className='col-5'>
-            <div className='footer-top-data d-flex gap-30 align-items-center'>
-              <img src={newsletter} alt="newsletter"/>
-              <h2 className='mb-0 text-white'>Sign up for newsletter</h2>
+    <>
+      <footer className="py-4">
+        <div className="container-xxl">
+          <div className="row align-items-center">
+            <div className="col-md-5">
+              <div className="footer-top-data d-flex gap-30 align-items-center">
+                <img src={newsletter} alt="newsletter" />
+                <h2 className="mb-0 text-white">Sign up for newsletter</h2>
+              </div>
             </div>
-          </div>
-          <div className='col-7'>
+            <div className="col-md-7 mt-3 mt-md-0">
               <div className="input-group">
                 <input
                   type="text"
                   className="form-control py-1"
                   placeholder="your email address"
                   aria-label="your email address"
-                  aria-describedby="basic-addon2" />
+                  aria-describedby="basic-addon2"
+                />
                 <span className="input-group-text p-2" id="basic-addon2">
                   subscribe
                 </span>
               </div>
             </div>
+          </div>
         </div>
-      </div>
-     </footer>
+      </footer>
 
-     {/* <footer className='py-3'>
+      {/* <footer className='py-3'>
        <div className='container-xxl'>
         <div className='row'>
           <div className='col-4'>
@@ -96,78 +97,144 @@ function Footer() {
        </div>
      </footer> */}
 
-<footer className='py-5 text-white'>
-  <div className='container-xl'>
-    <div className='row'>
-      <div className='col-lg-4 col-md-6 col-sm-12 mb-4'>
-        <h4 className='mb-4'>About Us</h4>
-        <p className='fs-6'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.</p>
-        <address className='text-muted fs-6 mt-3'>
-          hno: Mirpur-2,<br/>
-          Dhaka-1216<br/>
-          pincode:1190282
-        </address>
-        <div className='social-icons mt-4'>
-          <a href='#' className='text-white me-3'>
-            <BsLinkedin className='fs-4'/>
-          </a>
-          <a href='#' className='text-white me-3'>
-             <BsGithub className='fs-4'/>
-          </a>
-          <a href='#' className='text-white me-3'>
-            <BsYoutube className='fs-4'/>
-          </a>
-          <a href='#' className='text-white'>
-             <BsInstagram className='fs-4'/>
-          </a>
-        </div>
-      </div>
-      <div className='col-lg-3 col-md-6 col-sm-12 mb-4'>
-         <h4 className='mb-4'>Information</h4>
-         <ul className='list-unstyled'>
-          <li><Link to='/privacy-policy' className='text-muted mb-2 text-decoration-none'>Privacy Policy</Link></li>
-          <li><Link to='/refund-policy' className='text-muted mb-2 text-decoration-none'>Refund Policy</Link></li>
-          <li><Link to='/shipping-policy' className='text-muted mb-2 text-decoration-none'>Shipping Policy</Link></li>
-          <li><Link to='/terms-and-condition' className='text-muted mb-2 text-decoration-none'>Terms & Condition</Link></li>
-          <li><Link to='/blogs' className='text-muted mb-2 text-decoration-none'>Blogs</Link></li>
-        </ul>
-      </div>
-      <div className='col-lg-3 col-md-6 col-sm-12 mb-4'>
-        <h4 className='mb-4'>Contact</h4>
-        <ul className='list-unstyled'>
-          <li><Link className='text-muted mb-2 text-decoration-none'>About us</Link></li>
-          <li><Link className='text-muted mb-2 text-decoration-none'>Faq</Link></li>
-          <li><Link className='text-muted mb-2 text-decoration-none'>Contact</Link></li>
-        </ul>
-      </div>
-      <div className='col-lg-2 col-md-6 col-sm-12'>
-        <h4 className='mb-4'>Quick Links</h4>
-        <ul className='list-unstyled'>
-          <li><Link className='text-muted mb-2 text-decoration-none'>Laptops</Link></li>
-          <li><Link className='text-muted mb-2 text-decoration-none'>Headphones</Link></li>
-          <li><Link className='text-muted mb-2 text-decoration-none'>Tablet</Link></li>
-          <li><Link className='text-muted mb-2 text-decoration-none'>Watch</Link></li>
-        </ul>
-      </div>
-    </div>
-  </div>
-</footer>
-
-
-
-     <footer className='py-4'>
-      <div className='container-xxl'>
-        <div className='row'>
-          <div className='col-12'>
-            <p className='text-center  mb-0 text-white'>
-              &copy; {new Date().getFullYear()}; powered by Rajon
-            </p>
+      <footer className="py-5 text-white">
+        <div className="container-xl">
+          <div className="row">
+            <div className="col-lg-4 col-md-6 col-sm-12 mb-4">
+              <h4 className="mb-4">About Us</h4>
+              <p className="fs-6">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+                nec odio. Praesent libero.
+              </p>
+              <address className="text-muted fs-6 mt-3">
+                hno: Mirpur-2,
+                <br />
+                Dhaka-1216
+                <br />
+                pincode:1190282
+              </address>
+              <div className="social-icons mt-4">
+                <a href="#" className="text-white me-3">
+                  <BsLinkedin className="fs-4" />
+                </a>
+                <a href="#" className="text-white me-3">
+                  <BsGithub className="fs-4" />
+                </a>
+                <a href="#" className="text-white me-3">
+                  <BsYoutube className="fs-4" />
+                </a>
+                <a href="#" className="text-white">
+                  <BsInstagram className="fs-4" />
+                </a>
+              </div>
+            </div>
+            <div className="col-lg-3 col-md-6 col-sm-12 mb-4">
+              <h4 className="mb-4">Information</h4>
+              <ul className="list-unstyled">
+                <li>
+                  <Link
+                    to="/privacy-policy"
+                    className="text-muted mb-2 text-decoration-none"
+                  >
+                    Privacy Policy
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/refund-policy"
+                    className="text-muted mb-2 text-decoration-none"
+                  >
+                    Refund Policy
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/shipping-policy"
+                    className="text-muted mb-2 text-decoration-none"
+                  >
+                    Shipping Policy
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/terms-and-condition"
+                    className="text-muted mb-2 text-decoration-none"
+                  >
+                    Terms & Condition
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/blogs"
+                    className="text-muted mb-2 text-decoration-none"
+                  >
+                    Blogs
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <div className="col-lg-3 col-md-6 col-sm-12 mb-4">
+              <h4 className="mb-4">Contact</h4>
+              <ul className="list-unstyled">
+                <li>
+                  <Link className="text-muted mb-2 text-decoration-none">
+                    About us
+                  </Link>
+                </li>
+                <li>
+                  <Link className="text-muted mb-2 text-decoration-none">
+                    Faq
+                  </Link>
+                </li>
+                <li>
+                  <Link className="text-muted mb-2 text-decoration-none">
+                    Contact
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <div className="col-lg-2 col-md-6 col-sm-12">
+              <h4 className="mb-4">Quick Links</h4>
+              <ul className="list-unstyled">
+                <li>
+                  <Link className="text-muted mb-2 text-decoration-none">
+                    Laptops
+                  </Link>
+                </li>
+                <li>
+                  <Link className="text-muted mb-2 text-decoration-none">
+                    Headphones
+                  </Link>
+                </li>
+                <li>
+                  <Link className="text-muted mb-2 text-decoration-none">
+                    Tablet
+                  </Link>
+                </li>
+                <li>
+                  <Link className="text-muted mb-2 text-decoration-none">
+                    Watch
+                  </Link>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
-      </div>
-     </footer>
+      </footer>
+
+      <footer className="py-4">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <p className="text-center  mb-0 text-white">
+                &copy; {new Date().getFullYear()}; powered by Rajon
+              </p>
+            </div>
+          </div>
+        </div>
+      </footer>
     </>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -168,25 +168,26 @@ function Home() {
         </div>
       </Container>
 
-      <Container class1="home-wrapper-2 py-5">
-        <div className="row">
-          <div className="col-12">
-            <div className="services d-flex align-items-center justify-content-between">
-              {services?.map((i, j) => {
-                return (
-                  <div className="d-flex align-items-center gap-15" key={j}>
-                    <img src={i.image} alt="services" />
-                    <div>
-                      <h6>{i.title}</h6>
-                      <p className="mb-0">{i.tagline}</p>
+      <Container className="home-wrapper-2 py-5">
+    <div className="row">
+        <div className="col-12">
+            <div className="services d-flex flex-wrap justify-content-center align-items-center">
+                {services?.map((service, index) => (
+                    <div className="col-lg-3 col-md-6 col-sm-12 col-12 mb-4" key={index}>
+                        <div className="d-flex align-items-center gap-15">
+                            <img src={service.image} alt="services" />
+                            <div>
+                                <h6>{service.title}</h6>
+                                <p className="mb-0">{service.tagline}</p>
+                            </div>
+                        </div>
                     </div>
-                  </div>
-                );
-              })}
+                ))}
             </div>
-          </div>
         </div>
-      </Container>
+    </div>
+</Container>
+
 
       <Container class1="home-wrapper-2 py-5">
         <div className="row">
