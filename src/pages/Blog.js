34d3d33d4b05e8e9aedc9735d6c0acd46b7ab68f
rajon-git <rajon.zhsust15@gmail.com>
2024-05-07 23:08:43 +0600

    blog make responsive

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -23,41 +23,40 @@ function Blog() {
       <Meta title={"Blogs"} />
       <BreadCrumb title="Blogs" />
       <Container class1="blog-wrapper home-wrapper-2 py-5">
-        <div className="row">
-          <div className="col-3">
-            <div className="filter-card mb-3">
-              <h3 className="filter-title">Find By Categories</h3>
-              <div>
-                <ul className="ps-0">
-                  <li>Watch</li>
-                  <li>TV</li>
-                  <li>Camera</li>
-                  <li>Laptop</li>
-                </ul>
-              </div>
-            </div>
-          </div>
-          <div className="col-9">
-            <div className="row">
-              {blogState && blogState?.map((item, index) => {
-                return (
-                  <div className="col-6 mb-3" key={index}>
-                    <BlogCard
-                      id={item?._id}
-                      title={item?.title}
-                      description={item?.description}
-                      image={item?.images[0]?.url}
-                      date={moment(item?.createdAt).format(
-                        "MMMM Do YYYY, h:mm a"
-                      )}
-                    />
-                  </div>
-                );
-              })}
-            </div>
-          </div>
+  <div className="row">
+    <div className="col-lg-3 col-md-4 col-sm-6"> {/* Adjust column size based on screen size */}
+      <div className="filter-card mb-3">
+        <h3 className="filter-title">Find By Categories</h3>
+        <div>
+          <ul className="ps-0">
+            <li>Watch</li>
+            <li>TV</li>
+            <li>Camera</li>
+            <li>Laptop</li>
+          </ul>
         </div>
-      </Container>
+      </div>
+    </div>
+    <div className="col-lg-9 col-md-8 col-sm-6"> {/* Adjust column size based on screen size */}
+      <div className="row">
+        {blogState && blogState?.map((item, index) => {
+          return (
+            <div className="col-lg-6 col-md-6 col-sm-12 mb-3" key={index}> {/* Adjust column size based on screen size */}
+              <BlogCard
+                id={item?._id}
+                title={item?.title}
+                description={item?.description}
+                image={item?.images[0]?.url}
+                date={moment(item?.createdAt).format("MMMM Do YYYY, h:mm a")}
+              />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  </div>
+</Container>
+
     </>
   );
 }
